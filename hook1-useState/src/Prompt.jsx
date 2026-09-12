e9ecca4e57b0164f5114d366e4049e3ab5b4a2e1
rokadrajal <rokadrajal@gmail.com>
2026-09-12 10:42:30 +0530

    useState

--- hook1-useState/src/Prompt.jsx
+++ hook1-useState/src/Prompt.jsx
@@ -14,13 +14,12 @@ function Prompt() {
                 <button onClick={()=>{setfname(prompt("Enter your first name :"))}}>First name</button>
                 <button onClick={()=>{setlname(prompt("Enter your last name :"))}}>Last name</button>
                 <button onClick={()=>{setage(prompt("Enter your age :"))}}>Age</button>
-                <button onClick={()=>{setIs18(age>=18)}}>Click 18+</button>
+                <button onClick={()=>{setIs18(age>=18)}}>Check 18+</button>
             </div>
             <h1>Full Name : {fname} {lname}</h1>
             <h2>Age : {age}</h2>
             <h2>Is18+ : {(is18) ? "True": "False"}</h2>
         </>
-
     )
 }
 
